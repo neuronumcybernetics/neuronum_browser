095cfb270f51c372d0a9ccf779b52166198d2618
Neuronum <welcome@neuronum.net>
2025-09-24 11:49:14 +0200

    update frontend custom alert

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ server = Flask(__name__, static_folder='./static', template_folder='./templates'
 
 @server.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @server.route('/connect_cell')
 def connect_cell():
@@ -20,13 +20,17 @@ def cellkey():
 def create_cell():
     return render_template('create_cell.html')
 
-@server.route('/nodes')
-def nodes():
-    return render_template('nodes.html')
+@server.route('/create_business_cell')
+def create_business_cell():
+    return render_template('create_business_cell.html')
 
-@server.route('/neuronum_cellai')
-def neuronum_cellai():
-    return render_template('neuronum_cellai.html')
+@server.route('/create_community_cell')
+def create_community_cell():
+    return render_template('create_community_cell.html')
+
+@server.route('/browser')
+def browser():
+    return render_template('browser.html')
 
 def run_flask():
     server.run(host='127.0.0.1', port=55000, debug=False)
@@ -36,6 +40,6 @@ if __name__ == '__main__':
     flask_thread.start()
 
 
-    webview.create_window('Neuronum CELLai v1.0.2','http://127.0.0.1:55000',width=1280,height=800,resizable=True)
+    webview.create_window('Neuronum Browser v1.0.0','http://127.0.0.1:55000',width=1280,height=800,resizable=True)
     webview.start()
 
