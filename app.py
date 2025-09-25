7ea285b651d4f9bf36fb9fb6c5245ddf74dc4fb3
Neuronum <welcome@neuronum.net>
2025-09-25 15:53:36 +0200

    frontend design

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import webview
 import threading
+from pathlib import Path
+from dotenv import load_dotenv
+import os
+import requests
 
 server = Flask(__name__, static_folder='./static', template_folder='./templates')
 
 @server.route('/')
-def home():
+def home():  
     return render_template('index.html')
 
 @server.route('/connect_cell')
 def connect_cell():
+    credentials_folder_path = Path.home() / ".neuronum"
+    env_path = credentials_folder_path / ".env"
+
+    if env_path.exists():
+        load_dotenv(dotenv_path=env_path)
+        email = os.getenv('EMAIL')
+        password = os.getenv('PASSWORD')
+        
+        if email and password:
+            print(email, password)
+            return render_template('connect_cell.html', email=email, password=password)
+            
     return render_template('connect_cell.html')
 
 @server.route('/cellkey')
@@ -27,6 +43,37 @@ def create_community_cell():
 @server.route('/browser')
 def browser():
     return render_template('browser.html')
+
+
+@server.route('/store_credentials', methods=['POST'])
+def store_credentials():
+    data = request.get_json()
+    if data:
+        email = data.get('email')
+        host = data.get('host')
+        password = data.get('password')
+        synapse = data.get('synapse')
+        
+        if not email or not host:
+            return jsonify({'status': 'error', 'message': 'Missing email or host data'}), 400
+
+        try:
+            credentials_folder_path = Path.home() / ".neuronum"
+            credentials_folder_path.mkdir(parents=True, exist_ok=True)
+            
+            env_path = credentials_folder_path / ".env"
+            env_path.write_text(f"HOST={host}\nPASSWORD={password}\nNETWORK=neuronum.net\nSYNAPSE={synapse}\nEMAIL={email}")
+            
+            print(f"Credentials saved: Email: {email}, Host: {host}")
+            
+            return jsonify({'status': 'success', 'message': 'Credentials saved successfully'})
+        
+        except Exception as e:
+            print(f"Error saving credentials: {e}")
+            return jsonify({'status': 'error', 'message': 'Failed to save credentials'}), 500
+    
+    return jsonify({'status': 'error', 'message': 'Invalid data'}), 400
+        
 
 def run_flask():
     server.run(host='127.0.0.1', port=55000, debug=False)
